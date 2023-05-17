6d00b6471c95e49cad49400d764d59839e769683
micbuffa <micbuffa@gmail.com>
2023-05-17 08:05:06 +0200

    pagination

--- model/assignment.js
+++ model/assignment.js
@@ -1,5 +1,6 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
 let AssignmentSchema = Schema({
     id: Number,
@@ -7,6 +8,8 @@ let AssignmentSchema = Schema({
     nom: String,
     rendu: Boolean
 });
+
+AssignmentSchema.plugin(aggregatePaginate);
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
 // le nom de la collection (par défaut assignments) sera au pluriel, 
